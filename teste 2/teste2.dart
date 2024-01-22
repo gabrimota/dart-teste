601a6a82e9ff483470506657a0d3d59166d2b457
Gabriel Bomfim <gabrielbomfimmotadasilva@gmail.com>
2024-01-22 17:01:39 -0300

    código 2

---
+++ teste 2/teste2.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';   ---> nova biblioteca 
+
+void main(){
+  Map <String, dynamic> dadoss = json.decode(dados());
+    print(dadoss["nome"]);
+}
+String dados() {
+   return """
+    {
+  "nome": "Gabriel",
+  "Sobremome": "Bomfim Mota da Silva",
+  "cursos": [
+   { "curso": "dart",
+     "dificuldade": 1
+   },
+    { "curso": "Flutter",
+     "dificuldade": 2
+   }
+  ],
+  "endereço" : {
+    "Cidade" : "São Paulo",
+    "Rua": "Rua Alfonso sei la",
+    "Número": 149,
+    "Caso não encontrar ligar para": 1324141241
+   }
+   } 
+  """;
+  //dart junto com json
